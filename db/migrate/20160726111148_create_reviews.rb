aacd7ac9995cb529207b6518b04b2a6f9875a3db
ben yang <benyang00@gmail.com>
2016-07-27 11:18:22 +0800

    new migrations file

--- db/migrate/20160726111148_create_reviews.rb
+++ db/migrate/20160726111148_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
-      t.references :user, foreign_key: true
-      t.references :product, foreign_key: true
+      t.references :user
+      t.references :product
       t.text :content
 
       t.timestamps

546e328a9e7e96c9245d7a5cf8237861e71bec1a
ben yang <benyang00@gmail.com>
2016-07-27 16:24:51 +0800

    added staff login authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    staff = Staff.find_by(email: params[:session][:email].downcase)
+    if staff && staff.authenticate(params[:session][:password])
+      redirect_to('/users')
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,7 @@
 module SessionsHelper
+
+  def log_in(staff)
+    session[:user_id] = user.id
+  end
+
 end
